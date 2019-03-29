4eec6c3b6687992557980ab8d25dac7114bd8f5e
root <root@ip-172-31-18-153.us-east-2.compute.internal>
2019-03-29 21:11:03 +0000

    Feature: server log file

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const querystring = require('querystring');
 const fs = require('fs');
 const path = require('path');
 const os = require('os');
+var bodyParser = require('body-parser');
 
 //Sleeps for the specified time period without affecting other threads.
 const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
@@ -14,7 +15,15 @@ const asyncHandler = fn =>
     (req, res, next) =>  
     {   
         Promise.resolve(fn(req, res, next)).catch(function(error){   
-            console.log(error); 
+			console.log(error);
+			if(error.name == "InvalidInput"){
+				res.send(`{
+				"type":"error",
+				"message":"${error.message}"
+				}`);
+				res.end();
+				return;
+			}
             next();
         });
     };  
@@ -38,6 +47,11 @@ app.use(function (req, res, next) {
   console.log(req.url);
   next();
 });
+app.use(bodyParser.urlencoded(
+{
+	 extended: true 
+}));
+app.use(bodyParser.json());
 
 
 //Static Resource Serving
@@ -98,8 +112,56 @@ app.get('/test-server/ad-server/request.php', asyncHandler(async function(req, r
 	printFile(filename, req, res);
 	res.end();
 }));
+//Request Endpoint
+app.get('/test.php', asyncHandler(async function(req, res) {
+	res.type('json');
+	//Get Zone id
+	res.send(
+	`{"andrew":"chode"}`);
+	res.end();
+	return;
+}));
+function validateString(input, message)
+{
+	if(typeof input === "undefined" || !input){
+		var myError = new Error(message);
+		myError.name = "InvalidInput";
+		throw myError;
+	}
+}
+app.post('/tweet.php', asyncHandler(async function(req, res) {
+	res.type('json');
+	//TODO always console log post data
+	console.log(req.body);
+	var password = req.body.password;
+	var username = req.body.username;
+	var tweet = req.body.tweet;
+	validateString(password, "Please set Password");
+	validateString(username, "Please set Username");
+	validateString(tweet, "Please set a Tweet!");
+	//do data validation here;
+	const twitter = require("twitter");
+	await twitter.postOnTwitter(username, password, tweet, uploadFile = false, randomFollow = false).catch(function(error){
+		console.log(error);
+		console.log(error.name);
+		if(error.name == "TimeoutError"){
+			res.send(`{
+			"type":"error",
+			"message":"Invalid credentials!"
+			}`);
+			res.end();
+			return;
+		}
+	});
+	res.send(`{
+	"type":"success",
+	"message":"Tweet has been successfully scheduled!"
+	}`);
+	res.end();
+	return;
+}));
 
 let server = app.listen(8080, function() {  
-    console.log('Server is listening on port 8080')
+	console.log("Server is listening on port 8080");
 });
 
--- run.command
+++ run.command
@@ -1,6 +1,10 @@
+#force kill any existing node server running on the port
+pkill -9 chrome
+pkill -9 node
+> server.log
 #cd to the directory this file is in
 DIRECTORY=$(dirname "$0")
 cd "$DIRECTORY"
 
 #Start the node server
-nodemon app.js
+nodemon app.js 2>&1 | tee -a server.log
